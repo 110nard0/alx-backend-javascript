32fd5b9ba3e291fa1c29b022a53dd54de469939f
psych0wh1z <ikeagudike@gmail.com>
2023-07-26 19:26:09 +0100

    Fix linter error

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,5 @@
 export default function createIteratorObject(report) {
   const employees = [];
-
   for (const department in report.allEmployees) {
     for (const employee of report.allEmployees[department]) {
       employees.push(employee);
