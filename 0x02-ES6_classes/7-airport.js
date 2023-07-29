84b7a3e40fb2c31fe034eb07b28a47eb2d8db483
psych0wh1z <ikeagudike@gmail.com>
2023-07-29 11:29:10 +0100

    Add task7 files

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,35 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('name must be a string');
+    this._name = val;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    if (typeof val !== 'string') throw new TypeError('code must be a string');
+    this._code = val;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
