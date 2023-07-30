9e32017a3770e319c870d57ffb150131208836a2
psych0wh1z <ikeagudike@gmail.com>
2023-07-30 21:04:09 +0100

    Fix promie return value to promise array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,24 +1,20 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
+  const promises = [userPromise, photoPromise];
 
-  const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
-
-  const resultArray = [
-    { status: userResult.status, value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason },
-    { status: photoResult.status, value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason },
-  ];
-
-  return resultArray;
+  const resultArray = [];
+  return Promise.allSettled(promises)
+    .then((results) => {
+      results.forEach((result) => {
+        resultArray.push({
+          status: result.status,
+          value: result.status === 'fulfilled' ? result.value : result.reason,
+        });
+      });
+      return resultArray;
+    });
 }
-
-/*
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return signUpUser(firstName, lastName)
-    .then(() => uploadPhoto(fileName))
-    .catch((err) => err);
-}
-*/
