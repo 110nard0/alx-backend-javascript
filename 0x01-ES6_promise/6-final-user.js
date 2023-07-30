d3b7674320561bec950bcc9b10785a3f8acdf6e2
psych0wh1z <ikeagudike@gmail.com>
2023-07-30 20:25:43 +0100

    Test async function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,24 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+  const resultArray = [
+    { status: userResult.status, value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason },
+    { status: photoResult.status, value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason },
+  ];
+
+  return resultArray;
+}
+
+/*
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return signUpUser(firstName, lastName)
     .then(() => uploadPhoto(fileName))
-    .catch((err) => err)
+    .catch((err) => err);
 }
+*/
