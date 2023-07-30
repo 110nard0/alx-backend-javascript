edc90f80957808202365621f4856579dc0892c82
psych0wh1z <ikeagudike@gmail.com>
2023-07-30 21:17:14 +0100

    Change promise structure

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,17 +4,16 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
-  const promises = [userPromise, photoPromise];
 
-  const resultArray = [];
-  return Promise.allSettled(promises)
-    .then((results) => {
-      results.forEach((result) => {
-        resultArray.push({
-          status: result.status,
-          value: result.status === 'fulfilled' ? result.value : result.reason,
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((promises) => {
+      const result = [];
+      promises.forEach((promise) => {
+        result.push({
+          status: promise.status,
+          value: promise.status === 'fulfilled' ? promise.value : promise.reason,
         });
       });
-      return resultArray;
+      return result;
     });
 }
