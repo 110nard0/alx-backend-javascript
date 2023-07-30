b361f51d482d7e62a74d350406a6c513aefc3090
psych0wh1z <ikeagudike@gmail.com>
2023-07-30 18:58:06 +0100

    Revert "Refactor promise code"
    This reverts commit 00ef39b90ba6ca29b275481d4d5ad1b96e489766.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return new Promise((resolve) => {
+    resolve(promise);
+  })
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => {
