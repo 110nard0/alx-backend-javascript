44ff1971de8284030f777ed3f231fb2e2b8923bc
psych0wh1z <ikeagudike@gmail.com>
2023-07-31 23:14:49 +0100

    Fix linter errors

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 export default function cleanSet(set, startString) {
   const len = startString.length;
-  let endStrings = [];
+  const endStrings = [];
 
   if (len > 0) {
     set.forEach((value) => {
       if (value.startsWith(startString)) {
         endStrings.push(value.slice(len));
       }
-    })
+    });
   }
 
   return endStrings.join('-');
