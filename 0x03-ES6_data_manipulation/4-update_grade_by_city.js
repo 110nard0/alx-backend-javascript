3b9de4973329afaf849b30017fd99ec258c37a38
psych0wh1z <ikeagudike@gmail.com>
2023-07-31 13:21:19 +0100

    Add task4 files

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+export default function updateStudentGradeByCity(studentArray, city, newGrades) {
+  return studentArray
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentIds = newGrades.map((grade) => grade.studentId);
+
+      if (studentIds.includes(student.id)) {
+        newGrades.forEach((gradeBook) => {
+          if (student.id === gradeBook.studentId) {
+            student.grade = gradeBook.grade;
+          }
+        });
+      } else {
+        student.grade = 'N/A';
+      }
+      return student;
+    });
+}
