73deff24720b4926d52de4d77f5353419c5f0caa
psych0wh1z <ikeagudike@gmail.com>
2023-08-01 01:02:31 +0100

    Fix linter errors

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (startString === '' || startString === undefined) return '';
 
   return [...set]
-    .filter((string) => string.startsWith(startString) ? string : '')
-    .map((string) => string.slice(startString.length))
+    .filter((string) => (string !== undefined ? string.startsWith(startString) : ''))
+    .map((string) => (string !== undefined ? string.slice(startString.length) : ''))
     .join('-');
 }
