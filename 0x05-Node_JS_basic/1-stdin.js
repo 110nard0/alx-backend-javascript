717b88addd5a8255dc6805d4a95b8aa03fa06197
psych0wh1z <ikeagudike@gmail.com>
2023-09-20 08:56:29 +0100

    Fix function output

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,11 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.question('', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  rl.close();
-});
+if (!process.stdin.isTTY) {
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
