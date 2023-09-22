4672b05ea5e6dec367a3d0af88ed0102f1575203
psych0wh1z <ikeagudike@gmail.com>
2023-09-22 05:57:27 +0100

    Add task solutions

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+/* eslint-disable  */
+const fs = require('fs');
+
+module.exports = async function countStudents(filePath) {
+  try {
+    const content = fs.readFile(filePath, encoding = 'utf-8');
+    let lines = content.split('\n');
+    lines = lines.slice(1).slice(0, -1);
+    const len = lines.length;
+    const fieldset = {};
+
+    for (const line of lines) {
+      if (line) {
+        const data = line.split(',');
+        const field = data[3];
+
+        if (!(field in fieldset)) {
+          fieldset[field] = [];
+        }
+        fieldset[field].push(data[0]);
+      }
+    }
+
+    console.log(`Number of students: ${len}`);
+
+    for (const key in fieldset) {
+      nameList = fieldset[key];
+      console.log(`Number of students in ${key}: ${nameList.length}. List: ${nameList.join(', ')}`);
+    }
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+};
