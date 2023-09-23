a7e3e39ea14d274d201783804e093474a2367ea9
psych0wh1z <ikeagudike@gmail.com>
2023-09-23 05:19:59 +0100

    Fix Prommise return

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,32 +2,34 @@
 const fs = require('fs');
 
 module.exports = async function countStudents(filePath) {
-  try {
-    const content = fs.readFile(filePath, encoding = 'utf-8');
-    let lines = content.split('\n');
-    lines = lines.slice(1).slice(0, -1);
-    const len = lines.length;
-    const fieldset = {};
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, encoding = 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+          let lines = data.split('\n');
+          lines = lines.slice(1).filter(line => line.length > 1);
+          const len = lines.length;
+          const fieldset = {};
 
-    for (const line of lines) {
-      if (line) {
-        const data = line.split(',');
-        const field = data[3];
+          for (const line of lines) {
+            const content = line.split(',');
+            const field = content[3];
 
-        if (!(field in fieldset)) {
-          fieldset[field] = [];
-        }
-        fieldset[field].push(data[0]);
+            if (!(field in fieldset)) {
+              fieldset[field] = [];
+            }
+            fieldset[field].push(content[0]);
+          }
+
+          console.log(`Number of students: ${len}`);
+
+          for (const key in fieldset) {
+            nameList = fieldset[key];
+            console.log(`Number of students in ${key}: ${nameList.length}. List: ${nameList.join(', ')}`);
+          }
+          resolve(data);
       }
-    }
-
-    console.log(`Number of students: ${len}`);
-
-    for (const key in fieldset) {
-      nameList = fieldset[key];
-      console.log(`Number of students in ${key}: ${nameList.length}. List: ${nameList.join(', ')}`);
-    }
-  } catch (e) {
-    throw new Error('Cannot load the database');
-  }
+    });
+  });
 };
