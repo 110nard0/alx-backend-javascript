0d19c6110a6724b5da9c1ba4bbe383d24d552a6c
psych0wh1z <ikeagudike@gmail.com>
2023-09-23 07:35:21 +0100

    Fix lint errors

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,10 +3,12 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-module.exports = app = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname);
+
+module.exports = app;
