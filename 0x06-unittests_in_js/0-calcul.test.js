7a0127aee516b19e67d6ed28b36b508b4c2896dc
psych0wh1z <ikeagudike@gmail.com>
2023-09-23 21:03:01 +0100

    Rewrite test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,24 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function() {
-  it('should return 4 for input (1, 3)', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+  it('it should round the first argument', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
   });
 
-  it('should return 5 for input (1, 3.7)', function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it('it should round the second argument', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
   });
 
-  it('should return 5 for input (1.2, 3.7)', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it('it should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
   });
-
-  it('should return 6 for input (1.5, 3.7)', function() {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });        
 });
