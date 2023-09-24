ced77552b9e158e6b69ece7f2fbc73cbb311581d
psych0wh1z <ikeagudike@gmail.com>
2023-09-24 07:12:50 +0100

    Verify all tests pass

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', function() {
   describe('SUM', function() {
@@ -33,22 +33,22 @@ describe('calculateNumber', function() {
       assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
     });
 
-    it('should return -3 for input (SUBTRACT, 1.5, 3.7)', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -3);
+    it('should return -2 for input (SUBTRACT, 1.5, 3.7)', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
     });
   });
 
   describe('DIVIDE', function() {
-    it('should return 0.33 for input (DIVIDE, 1, 3)', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1, 3), 0.33);
+    it('should return 0.5 for input (DIVIDE, 1, 2)', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 2), 0.5);
     });
 
-    it('should return 0.27 for input (DIVIDE, 1, 3.7)', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1, 3.7), 0.27);
+    it('should return 0.25 for input (DIVIDE, 1, 3.7)', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 3.7), 0.25);
     });
 
-    it('should return 0.32 for input (DIVIDE, 1.2, 3.7)', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.2, 3.7), 0.32);
+    it('should return 0.5 for input (DIVIDE, 2.2, 3.7)', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 2.2, 3.7), 0.5);
     });
 
     it('should return "Error" for input (DIVIDE, 1.5, 0)', function() {
