38e3a457a0d302313eb1634fc4081234776dab07
psych0wh1z <ikeagudike@gmail.com>
2023-09-25 04:55:08 +0100

    Add task6 script and test files

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,11 @@
+const getPaymentTokenFromAPI = (success) => {
+  if (success) {
+    return Promise.resolve({data: 'Successful response from the API'});
+  } else {
+    return Promise.resolve();
+  }
+}
+
+module.exports = {
+  getPaymentTokenFromAPI
+};
---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const { getPaymentTokenFromAPI } = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should resolve with correct data when success is true', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.deep.equal({data: 'Successful response from the API'});
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+
+  it('should resolve with undefined when success is false', function(done) {
+    getPaymentTokenFromAPI(false)
+      .then((response) => {
+        expect(response).to.be.undefined;
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+});
