b72c10f8bb7894fe874e14e770a2143a1a9fa472
psych0wh1z <ikeagudike@gmail.com>
2023-09-25 10:13:42 +0100

    Add task9 API script, test, and setup files

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,47 @@
+const { expect } = require('chai');
+const request = require('request');
+const url = 'http://localhost:7865';
+
+describe('make api call to index page', () => {
+  it('should have status code of 200 from index page', () => {
+    request.get(url, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+    });
+  });
+
+  it('should return correct welcome message', () => {
+    request.get(url, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+
+  it('should have content-length equal to 29', () => {
+    request.get(url, (error, response, body) => {
+      expect(response.headers['content-length']).to.equal('29');
+    });
+  });
+
+  it('should have correct status code when id is a number', () => {
+    request.get(`${url}/cart/12`, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+    });
+  });
+
+  it('should have correct status code when id is not a number', () => {
+    request.get(`${url}/cart/hello`, (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+    });
+  });
+
+  it('should have correct response message when id is a number', () => {
+    request.get(`${url}/cart/12`, (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 12');
+    });
+  });
+
+  it('should  have correct result when id is not a number', () => {
+    request.get(`${url}/cart/hello`, (error, response, body) => {
+      expect(body).to.include('Cannot GET /cart/hello');
+    });
+  });
+});
