632077d618c9b21fb25ccfa0939e3ebc9b58388c
psych0wh1z <ikeagudike@gmail.com>
2023-09-25 11:07:29 +0100

    Rewrite API test suite

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -7,37 +7,42 @@ describe("Index page", function() {
     url: "http://localhost:7865/",
     method: "GET"
   }
-  it("check correct status code", function(done) {
-    request(options, function(err, res, body) {
+
+  it("check correct status code", () => {
+    request(options, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
-      done();
     });
   });
-  it("check correct content", function(done) {
-    request(options, function(err, res, body) {
+
+  it("check correct content", () => {
+    request(options, (err, res, body) => {
       expect(body).to.equal("Welcome to the payment system");
-      done();
+    });
+  });
+
+  it('check correct content-length', () => {
+    request(options, (err, res, body) => {
+      expect(res.headers['content-length']).to.equal('29');
     });
   });
 });
 
 describe("Cart page", function() {
-  it("check correct status code for correct url", function(done) {
-    request.get("http://localhost:7865/cart/12", function(err, res, body) {
+  it("check correct status code for correct url", () => {
+    request.get("http://localhost:7865/cart/12", (err, res, body) => {
       expect(res.statusCode).to.equal(200);
-      done();
     });
   });
-  it("check correct content for correct url", function(done) {
-    request.get("http://localhost:7865/cart/12", function(err, res, body) {
+
+  it("check correct content for correct url", () => {
+    request.get("http://localhost:7865/cart/12", (err, res, body) => {
       expect(body).to.contain("Payment methods for cart 12");
-      done();
     });
   });
-  it("check correct status code for incorrect url", function(done) {
-    request.get("http://localhost:7865/cart/kim", function(err, res, body) {
+
+  it("check correct status code for incorrect url", () => {
+    request.get("http://localhost:7865/cart/abc", (err, res, body) => {
       expect(res.statusCode).to.equal(404);
-      done();
     });
   });
 });
